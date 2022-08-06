3f685a84243cdfa220538483a2d35d945d098b42
JulyK9 <97942837+JulyK9@users.noreply.github.com>
2022-08-06 22:47:04 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+function App() {
+  const [popup, setPopup] = useState(false);
+
+  const handlePopup = (e) => {
+    setPopup(popup ? false : true);
+  };
+
+  return (
+    <div className="App">
+      <h1>Fix me to open Pop up</h1>
+      <button className="open" onClick={handlePopup}>
+        Open me
+      </button>
+      {popup ? (
+        <div className="popup">
+          <div className="popup_inner">
+            <h2>Success!</h2>
+            <button className="close" onClick={handlePopup}>
+              Close me
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
